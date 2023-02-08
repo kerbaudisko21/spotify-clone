93b0e92d581555378e65b54187ef2492f2fd2592
Wilbert <Wilbertw99@gmail.com>
2023-02-09 01:36:07 +0700

    login section done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import { useEffect } from 'react';
+import {useStateProvider} from './utils/StateProvider'
+import { reducerCases } from './utils/Constants';
+import Spotify from './components/Spotify';
 
 function App() {
+  const [{token}, dispatch] = useStateProvider();
+  useEffect(()=>{
+    const hash = window.location.hash;
+    if(hash){
+      const token = hash.substring(1).split("&")[0].split('=')[1];
+      console.log(token)
+      dispatch({type:reducerCases.SET_TOKEN, token});
+    }
+  },[token,dispatch])
+
   return (
-    <div>App</div>
+    <div>
+      {
+        token ? <Spotify /> : <Login />
+      }
+    </div>
   )
 }
 
